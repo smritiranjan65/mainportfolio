9de6a4c4e006dcd5be0d24993278d234af82c402
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2023-09-01 17:28:12 +0530

    Update settings.py

--- smriti/settings.py
+++ smriti/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-90!0y=0n3xn%#bqon(q#jp7xny_q^)b^0o4ud(er#!#k_s#0$$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , 'smriti.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
